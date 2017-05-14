0665f65c89bb0319c37a738f559d30056664ac22
Rohlem <lhoermann@student.tgm.ac.at>
2017-05-15 01:14:28 +0200

    moved circular buffer function declarations into separate header
    ... to further improve C++ interoperability

--- src/notelib/util/circular_buffer.h
+++ src/notelib/util/circular_buffer.h
@@ -1,6 +1,8 @@
 #ifndef NOTELIB_UTIL_CIRCULAR_BUFFER_H_
 #define NOTELIB_UTIL_CIRCULAR_BUFFER_H_
 
+#include "circular_buffer_interface.h"
+
 #include "../internal/circular_buffer.h"
 
 #include <stdalign.h>
@@ -15,7 +17,7 @@
 ///These are two thread-safe lock-free queue-like data structures.
 ///Please note: Due to a current design flaw/oddity (it's not that big of a deal) they require one buffer element, and so don't support taking advantage of all their data space ("empty" and "full" states were not differentiated).
 
-//Data size is always an integer multiple of element_size; only one element can be read and written at a time.
+//Data size is always an integer multiple of element_size and only one element can be read and written at a time => guarantees contiguous memory.
 struct circular_buffer{
 	//customization idea: "perfect solution" algorithm (halving max element_count) by "collaborative" "everything written" bit of XORed top bits of both positions
 	_Atomic uint16_t reader_position;
@@ -29,21 +31,6 @@ struct circular_buffer{
 enum {circular_buffer_header_size =
 	offsetof(struct circular_buffer, data)};
 
-size_t circular_buffer_sizeof(uint16_t element_size, uint16_t element_count);
-size_t circular_buffer_dynamic_sizeof(const struct circular_buffer* object);
-
-struct circular_buffer* circular_buffer_construct
-(void* position, uint32_t space, uint16_t element_size);
-
-bool        circular_buffer_read
-(      struct circular_buffer* object,       void* dest);
-const void* circular_buffer_direct_read_commence
-(const struct circular_buffer* object);
-void        circular_buffer_direct_read_commit
-(      struct circular_buffer* object);
-bool        circular_buffer_write
-(      struct circular_buffer* object, const void* src);
-
 //Data size is arbitrary; reading always happens by memcpy-ing into a caller-provided buffer.
 struct circular_buffer_liberal_reader_unsynchronized{
 	_Atomic uint32_t reader_position;
@@ -54,26 +41,5 @@ struct circular_buffer_liberal_reader_unsynchronized{
 
 enum {circular_buffer_liberal_reader_unsynchronized_header_size =
 	offsetof(struct circular_buffer_liberal_reader_unsynchronized, data)};
-
-size_t circular_buffer_liberal_reader_unsynchronized_sizeof
-(uint32_t data_size);
-size_t circular_buffer_liberal_reader_unsynchronized_dynamic_sizeof
-(const struct circular_buffer_liberal_reader_unsynchronized* object);
-
-struct circular_buffer_liberal_reader_unsynchronized*
-circular_buffer_liberal_reader_unsynchronized_construct
-(void* position, size_t space, uint32_t data_size);
-
-void circular_buffer_liberal_reader_unsynchronized_read
-(struct circular_buffer_liberal_reader_unsynchronized* object,
- uint32_t size,   void* dest);
-bool circular_buffer_liberal_reader_unsynchronized_write
-(struct circular_buffer_liberal_reader_unsynchronized* object,
- const void* src, uint32_t size);
-/*Although unwrite returns a bool it basically only contains a sanity check;
-  if it returns false there is an inconsistency in the system
-  and it is in what is pretty much undefined state.*/
-bool circular_buffer_liberal_reader_unsynchronized_unwrite
-(struct circular_buffer_liberal_reader_unsynchronized* object, uint32_t size);
 
 #endif//#ifndef NOTELIB_UTIL_CIRCULAR_BUFFER_H_
---
+++ src/notelib/util/circular_buffer_interface.h
@@ -0,0 +1,62 @@
+#ifndef NOTELIB_UTIL_CIRCULAR_BUFFER_INTERFACE_H_
+#define NOTELIB_UTIL_CIRCULAR_BUFFER_INTERFACE_H_
+
+/*basically the .h without struct definitions; could technically be named _fwd I guess;
+  its main use case is C++ interoperability, which doesn't understand _Atomic and would therefore screw up the structs' definitions*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif//#ifdef __cplusplus
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+//Data size is always an integer multiple of element_size and only one element can be read and written at a time => guarantees contiguous memory.
+struct circular_buffer;
+
+size_t circular_buffer_sizeof(uint16_t element_size, uint16_t element_count);
+size_t circular_buffer_dynamic_sizeof(const struct circular_buffer* object);
+
+struct circular_buffer* circular_buffer_construct
+(void* position, uint32_t space, uint16_t element_size);
+
+bool        circular_buffer_read
+(      struct circular_buffer* object,       void* dest);
+const void* circular_buffer_direct_read_commence
+(const struct circular_buffer* object);
+void        circular_buffer_direct_read_commit
+(      struct circular_buffer* object);
+bool        circular_buffer_write
+(      struct circular_buffer* object, const void* src);
+
+//Data size is arbitrary; reading always happens by memcpy-ing into a caller-provided buffer.
+struct circular_buffer_liberal_reader_unsynchronized;
+
+size_t circular_buffer_liberal_reader_unsynchronized_sizeof
+(uint32_t data_size);
+size_t circular_buffer_liberal_reader_unsynchronized_dynamic_sizeof
+(const struct circular_buffer_liberal_reader_unsynchronized* object);
+
+struct circular_buffer_liberal_reader_unsynchronized*
+circular_buffer_liberal_reader_unsynchronized_construct
+(void* position, size_t space, uint32_t data_size);
+
+void circular_buffer_liberal_reader_unsynchronized_read
+(struct circular_buffer_liberal_reader_unsynchronized* object,
+ uint32_t size,   void* dest);
+bool circular_buffer_liberal_reader_unsynchronized_write
+(struct circular_buffer_liberal_reader_unsynchronized* object,
+ const void* src, uint32_t size);
+/*Although unwrite returns a bool it basically only contains a sanity check;
+  if it returns false there is an inconsistency in the system
+  and it is in what is pretty much undefined state.*/
+bool circular_buffer_liberal_reader_unsynchronized_unwrite
+(struct circular_buffer_liberal_reader_unsynchronized* object, uint32_t size);
+
+#ifdef __cplusplus
+}
+#endif//#ifdef __cplusplus
+
+#endif//#ifndef NOTELIB_UTIL_CIRCULAR_BUFFER_INTERFACE_H_
